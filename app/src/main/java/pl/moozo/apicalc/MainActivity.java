32e72c74afe981a60e5c089a0f452d263917f64f
Peter <moozo89@gmail.com>
2017-10-28 13:33:32 +0200

    working api call

--- app/src/main/java/pl/moozo/apicalc/MainActivity.java
+++ app/src/main/java/pl/moozo/apicalc/MainActivity.java
@@ -2,12 +2,17 @@ package pl.moozo.apicalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import pl.moozo.apicalc.data.model.PostSum;
 import pl.moozo.apicalc.data.remote.APIService;
 import pl.moozo.apicalc.data.remote.ApiUtils;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,14 +25,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button sendBtn = (Button) findViewById(R.id.sendBtn);
+        responseTv = (TextView) findViewById(R.id.result);
 
         apiService = ApiUtils.getAPIService();
 
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                apiService.sum(6, 7);
+                postSum(6, 7);
             }
         });
+    }
+
+    public void postSum(long a, long b){
+        apiService.sum(a, b).enqueue(new Callback<PostSum>() {
+            @Override
+            public void onResponse(Call<PostSum> call, Response<PostSum> response) {
+                if(response.isSuccessful()) {
+                    showResponse(response.body().toString());
+                    Log.i("TAG", "post submitted to API." + response.body().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PostSum> call, Throwable t) {
+                Log.e("TAG", "Unable to submit post to API.");
+            }
+        });
+    }
+
+    public void showResponse(String response) {
+        if(responseTv.getVisibility() == View.GONE){
+            responseTv.setVisibility(View.VISIBLE);
+        }
+        responseTv.setText(response);
     }
 }
